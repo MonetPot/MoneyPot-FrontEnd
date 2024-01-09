1e707fb933c1390c096d3e2853953f019a3876ed
SaiSamarth123 <sai003sam@gmail.com>
2024-01-08 21:39:08 -0600

    "cleaned the code"

--- lib/screens/welcome/welcome_screen.dart
+++ lib/screens/welcome/welcome_screen.dart
@@ -1,73 +1,3 @@
-// import 'package:flutter/material.dart';
-//
-// import '../../components/background.dart';
-// import '../../responsive.dart';
-// import 'components/login_signup_btn.dart';
-// import 'components/welcome_image.dart';
-//
-// class WelcomeScreen extends StatelessWidget {
-//   const WelcomeScreen({Key? key}) : super(key: key);
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Background(
-//       child: SingleChildScrollView(
-//         child: SafeArea(
-//           child: Responsive(
-//             desktop: Row(
-//               mainAxisAlignment: MainAxisAlignment.end,
-//               children: [
-//                 const Expanded(
-//                   child: WelcomeImage(),
-//                 ),
-//                 Expanded(
-//                   child: Row(
-//                     mainAxisAlignment: MainAxisAlignment.center,
-//                     children: const [
-//                       SizedBox(
-//                         width: 450,
-//                         child: LoginAndSignupBtn(),
-//                       ),
-//                     ],
-//                   ),
-//                 ),
-//               ],
-//             ),
-//             mobile: const MobileWelcomeScreen(),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
-//
-// class MobileWelcomeScreen extends StatelessWidget {
-//   const MobileWelcomeScreen({
-//     Key? key,
-//   }) : super(key: key);
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Column(
-//       mainAxisAlignment: MainAxisAlignment.center,
-//       children: <Widget>[
-//         const WelcomeImage(),
-//         Row(
-//           children: const [
-//             Spacer(),
-//             Expanded(
-//               flex: 8,
-//               child: LoginAndSignupBtn(),
-//             ),
-//             Spacer(),
-//           ],
-//         ),
-//       ],
-//     );
-//   }
-// }
-
-
 import 'package:flutter/material.dart';
 
 import '../../components/background.dart';
@@ -146,41 +76,6 @@ class MobileWelcomeScreen extends StatelessWidget {
     );
   }
 
-  // @override
-  // Widget build(BuildContext context) {
-  //   return Scaffold(
-  //     body: Container(
-  //       padding: EdgeInsets.only(top: 64.0),
-  //       decoration: BoxDecoration(gradient: SIGNUP_BACKGROUND),
-  //       child: ListView(
-  //         physics: BouncingScrollPhysics(),
-  //         children: <Widget>[
-  //           // Center(
-  //           //   child: Image.asset(
-  //           //     'assets/icons/login.svg',
-  //           //     width: 100.0,
-  //           //     height: 100.0,
-  //           //     fit: BoxFit.cover,
-  //           //   ),
-  //           // ),
-  //           Center(
-  //             child: SvgPicture.asset(
-  //               'assets/icons/login.svg',
-  //               width: 100.0,
-  //               height: 100.0,
-  //             ),
-  //           ),
-  //           // headlinesWidget(),
-  //           // emailTextFieldWidget(),
-  //           // passwordTextFieldWidget(),
-  //           // loginButtonWidget(),
-  //           // signupWidget(context)
-  //         ],
-  //       ),
-  //     ),
-  //   );
-  // }
-
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -214,3 +109,75 @@ class MobileWelcomeScreen extends StatelessWidget {
     );
   }
 }
+
+
+
+// import 'package:flutter/material.dart';
+//
+// import '../../components/background.dart';
+// import '../../responsive.dart';
+// import 'components/login_signup_btn.dart';
+// import 'components/welcome_image.dart';
+//
+// class WelcomeScreen extends StatelessWidget {
+//   const WelcomeScreen({Key? key}) : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Background(
+//       child: SingleChildScrollView(
+//         child: SafeArea(
+//           child: Responsive(
+//             desktop: Row(
+//               mainAxisAlignment: MainAxisAlignment.end,
+//               children: [
+//                 const Expanded(
+//                   child: WelcomeImage(),
+//                 ),
+//                 Expanded(
+//                   child: Row(
+//                     mainAxisAlignment: MainAxisAlignment.center,
+//                     children: const [
+//                       SizedBox(
+//                         width: 450,
+//                         child: LoginAndSignupBtn(),
+//                       ),
+//                     ],
+//                   ),
+//                 ),
+//               ],
+//             ),
+//             mobile: const MobileWelcomeScreen(),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+//
+// class MobileWelcomeScreen extends StatelessWidget {
+//   const MobileWelcomeScreen({
+//     Key? key,
+//   }) : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Column(
+//       mainAxisAlignment: MainAxisAlignment.center,
+//       children: <Widget>[
+//         const WelcomeImage(),
+//         Row(
+//           children: const [
+//             Spacer(),
+//             Expanded(
+//               flex: 8,
+//               child: LoginAndSignupBtn(),
+//             ),
+//             Spacer(),
+//           ],
+//         ),
+//       ],
+//     );
+//   }
+// }
+

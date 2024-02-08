3a03c27ed55385de460cf1f18c711eacd4a3f727
SaiSamarth123 <sai003sam@gmail.com>
2024-02-07 20:37:12 -0600

    "changing main screen"

--- lib/screens/navigation.dart
+++ lib/screens/navigation.dart
@@ -1,4 +1,4 @@
-import 'dart:math';
+import 'dart:math' as math;
 
 import 'package:flutter/material.dart';
 import 'package:money_pot/screens/group/add_group.dart';
@@ -25,15 +25,14 @@ class _NavigationState extends State<Navigation> {
   String? userIdentifier;
 
   void _onNavBarTapped(int index) {
-    if (index == 1) {
-      _openAddGroupScreen(context);
-    } else {
-      _pageController.animateToPage(
-        index,
-        duration: Duration(milliseconds: 300),
-        curve: Curves.easeInOut,
-      );
-    }
+    // if (index == 1) {
+    //   _openAddGroupScreen(context);
+    // } else {
+    _pageController.animateToPage(
+      index,
+      duration: Duration(milliseconds: 300),
+      curve: Curves.easeInOut,
+    );
   }
 
   void initState() {
@@ -93,7 +92,7 @@ class _NavigationState extends State<Navigation> {
         onPageChanged: _onPageChanged,
         children: <Widget>[
           GroupsScreen(identifier: userIdentifier!),
-          AddGroup(),
+          // AddGroup(),
           UserScreen(),
         ],
       ),
@@ -173,37 +172,67 @@ class _ExpandableFabState extends State<ExpandableFab>
   }
 
   Widget _buildOption(IconData icon, double angle, double distance) {
-    final double rad = angle * (pi / 180.0);
-    final double dx = cos(rad) * distance;
-    final double dy = sin(rad) * distance;
+    final double rad = angle * (math.pi / 180.0);
+    final double dx = math.cos(rad) * distance;
+    final double dy = math.sin(rad) * distance;
     return Positioned(
-      bottom: 56.0 + (dy * _animation.value), // 56.0 is the default FAB size
+      bottom: 56.0 + dy * _animation.value, // 56.0 is the default FAB size
       right: (MediaQuery.of(context).size.width / 2 - 56.0 / 2) -
-          (dx * _animation.value),
+          dx * _animation.value,
       child: Transform(
         transform: Matrix4.identity()
-          ..translate(
-            (dx * _animation.value),
-            (dy * _animation.value),
-          ),
+          ..translate(dx * (1 - _animation.value), dy * (1 - _animation.value)),
         child: Opacity(
           opacity: _animation.value,
           child: CircularButton(
-            color: Colors.grey.shade800,
+            color: _animation.value > 0
+                ? Colors.blue
+                : Colors.grey.shade800, // Change color when visible
             width: 40,
             height: 40,
-            icon: Icon(
-              icon,
-              color: Colors.white,
-            ),
+            icon: Icon(icon, color: Colors.white),
             onClick: () {
               // Add your onPressed functionality
+              _toggle(); // Optionally close the FAB menu
             },
           ),
         ),
       ),
     );
   }
+
+  // Widget _buildOption(IconData icon, double angle, double distance) {
+  //   final double rad = angle * (pi / 180.0);
+  //   final double dx = cos(rad) * distance;
+  //   final double dy = sin(rad) * distance;
+  //   return Positioned(
+  //     bottom: 56.0 + (dy * _animation.value), // 56.0 is the default FAB size
+  //     right: (MediaQuery.of(context).size.width / 2 - 56.0 / 2) -
+  //         (dx * _animation.value),
+  //     child: Transform(
+  //       transform: Matrix4.identity()
+  //         ..translate(
+  //           (dx * _animation.value),
+  //           (dy * _animation.value),
+  //         ),
+  //       child: Opacity(
+  //         opacity: _animation.value,
+  //         child: CircularButton(
+  //           color: Colors.grey.shade800,
+  //           width: 40,
+  //           height: 40,
+  //           icon: Icon(
+  //             icon,
+  //             color: Colors.white,
+  //           ),
+  //           onClick: () {
+  //             // Add your onPressed functionality
+  //           },
+  //         ),
+  //       ),
+  //     ),
+  //   );
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -213,8 +242,8 @@ class _ExpandableFabState extends State<ExpandableFab>
       children: [
         _buildTapToCloseFab(),
         _buildOption(
-            Icons.text_fields, -90.0, 100), // Positioned left from the FAB
-        _buildOption(Icons.image, 90.0, 100), // Positioned right from the FAB
+            Icons.text_fields, 180.0, 100), // Positioned left from the FAB
+        _buildOption(Icons.image, 0.0, 100), // Positioned right from the FAB
       ],
     );
   }
@@ -256,12 +285,20 @@ class CircularButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(color: color, shape: BoxShape.circle),
+      decoration:
+          BoxDecoration(color: color, shape: BoxShape.circle, boxShadow: [
+        if (color == Colors.blue) // Shadow appears when the button is blue
+          BoxShadow(
+            color: Colors.blueAccent,
+            spreadRadius: 1,
+            blurRadius: 2,
+            offset: Offset(0, 2), // changes position of shadow
+          ),
+      ]),
       width: width,
       height: height,
       child: IconButton(
         icon: icon,
-        enableFeedback: true,
         onPressed: onClick,
       ),
     );
